5ed1fadc621d35065ce72e4e6bb3bf9bbeae3b19
cheng <easonwong0512@163.com>
2018-08-10 08:51:11 +0800

    添加了一个新的PlusNewClass类,测试

---
+++ app/src/main/java/com/solidndt/gittest/PlusNewClass.java
@@ -0,0 +1,11 @@
+package com.solidndt.gittest;
+
+import android.util.Log;
+
+public class PlusNewClass {
+
+    public void test (){
+
+        Log.i("chengGitHub", "test: 这是新添加的一个类,测试github");
+    }
+}
